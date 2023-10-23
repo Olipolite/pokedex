326a80f93286f41112467bc40943eec6ed08aa96
Oliver Oquist <o.oequist@sculpt.digital>
2023-10-23 16:44:50 +0100

    added fetch for shinyPokemon, added button for shinyPokemon, new font

--- script.js
+++ script.js
@@ -9,27 +9,59 @@ const fetchPokemon = () => {
             name: result.name,
             image: result.sprites['front_default'],
             type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
+            id: result.id,
         }));
         displayPokemon(pokemon);
+        console.log(pokemon)
     });
 };
 
+const fetchShinyPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemonShiny = results.map((result) => ({
+            image: result.sprites['front_shiny'],
+        }));
+        const cardImage = document.querySelectorAll(".card-image");
+        cardImage.forEach((img, index) => {
+            img.src = pokemonShiny[index].image;
+        })
+        console.log(pokemonShiny,"shiny");
+    });
+};
+
+fetchPokemon();
+
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
     const pokemonHTMLString = pokemon
-        .map(
-            (pokeman) => `
-        <li class="pokemon-card">
-            <img class="card-image" src="${pokeman.image}"/>
-            <h2 class="card-title"> ${pokeman.name.charAt(0).toUpperCase() + pokeman.name.slice(1)}</h2>
-            <p class="card-subtitle">Type: ${pokeman.type.charAt(0).toUpperCase() + pokeman.type.slice(1)}</p>
-        </li>
+        .map((pokeman) => `
+            <li class="pokemon-card">
+                <img class="card-image" src="${pokeman.image}"/>
+                <h2 class="card-title"> ${pokeman.name.charAt(0).toUpperCase() + pokeman.name.slice(1)}</h2>
+                <p class="card-subtitle">Type: ${pokeman.type.charAt(0).toUpperCase() + pokeman.type.slice(1)}</p>
+            </li>
     `
         )
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
 };
 
-fetchPokemon();
-extraText()
+const shinyButton = document.createElement("button");
+shinyButton.textContent = "Shiny!"
+const containerClass = document.querySelector(".container");
+containerClass.prepend(shinyButton);
+
+// Set ID for button -
+const setShinyButtonId = document.querySelector(".container").firstChild;
+setShinyButtonId.setAttribute("id", "shiny-button");
+
+const shinyToggle = () => { 
+    console.log("tjenare");
+    fetchShinyPokemon();
+};
+const buttonId = document.getElementById("shiny-button");
+buttonId.addEventListener("click", shinyToggle);
